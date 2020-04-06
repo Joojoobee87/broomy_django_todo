fcf13a45cbab0c0fff99c05f664ac8248ee855a7
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-04-06 14:24:23 +0000

    pushed changes

--- broomy_django_todo/settings.py
+++ broomy_django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'vwf%u1coqeh_a@6&rwg_v05c)xf(cb_g&5xw6td^dite*iwnp6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -74,13 +75,14 @@ WSGI_APPLICATION = 'broomy_django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
+DATABASES = {'default': dj_database_url.parse("postgres://hqxytjbtyghtel:870a6a143dce365cfb2a3fee1670fb9f7b8abf5cd4fed38a606d15be3c7a15db@ec2-46-137-84-173.eu-west-1.compute.amazonaws.com:5432/ddcpjq2k36ags2")}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
